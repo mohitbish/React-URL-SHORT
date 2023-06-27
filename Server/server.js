daee1fcee291e3d1571cd4e7e01b068065cb2d7e
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-27 20:37:49 +1000

    backend and frontend tallking

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 8888;
+var cors = require('cors')
 const MANGO_URL = "mongodb://localhost:27017/Url-short";
+var bodyParser = require('body-parser')
+
+
+
+app.use(cors())
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 mongoose
   .connect(MANGO_URL, {
@@ -15,5 +28,13 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+
+
+app.post('/submitroute', async(req, res)=>{
+  
+  console.log(req.body)
+
+});
+
 
 app.listen(PORT, () => console.log("Server is running on ", PORT));
--- url-short/src/App.jsx
+++ url-short/src/App.jsx
@@ -1,19 +1,46 @@
-function App() {
+import axios from "axios";
+import { useState } from "react";
 
-  const handlesubmit = () => {
-    console.log("working")
-  }
+function App() {
+  
+
+  const submitroute = 'http://localhost:8888/submitroute';
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handlesubmit = async () => {
+    try {
+      const response = await axios.post(submitroute, {
+        data: inputValue,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+      
+  };
 
   return (
     <div className=" w-[100vw] h-[100vh] container mx-auto my-auto flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold w-[70vw]">URL-SHORTNER</h1>
-      <form className="flex flex-col my-10 bg-[#2d3748]" >
+      <form className="flex flex-col my-10 bg-[#2d3748]">
         <input
           type="text"
+          name="FullURL"
           className="border rounded p-2 w-[50vw]  text-white  bg-gray-700"
           placeholder="Enter your url"
+          value={inputValue}
+          onChange={handleInputChange}
         />
-        <button onClick={handlesubmit} type="submit" className="bg-gray-900 text-white font-semibold uppercase py-2 rounded  hover:bg-gray-700">
+        <button
+          onClick={handlesubmit}
+          type="submit"
+          className="bg-gray-900 text-white font-semibold uppercase py-2 rounded  hover:bg-gray-700"
+        >
           Shrink
         </button>
       </form>
@@ -30,20 +57,19 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr >
+          <tr>
             <td className="w-3/5 bg-gray-200 border-r border-gray-300 py-2 px-4 hover:bg-gray-100">
               Content 1
             </td>
             <td className="w-2/5 bg-gray-200 border-r border-gray-300 py-2 px-4 hover:bg-gray-100">
               Content 2
             </td>
-            <td className="w-1/10 bg-gray-200 py-2 px-4 hover:bg-gray-100">Content 3</td>
+            <td className="w-1/10 bg-gray-200 py-2 px-4 hover:bg-gray-100">
+              Content 3
+            </td>
           </tr>
         </tbody>
       </table>
-
-      
-
     </div>
   );
 }
